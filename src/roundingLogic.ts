3aaa9e8f493a91dbda1b6ab19f17a30bff2e1aec
pmat0 <140417871+pmat0@users.noreply.github.com>
2023-07-24 16:41:08 +0200

    Update roundingLogic.ts
    some illegal activities with an Number object

--- src/roundingLogic.ts
+++ src/roundingLogic.ts
@@ -3,3 +3,16 @@
 export function r2p5( float: number ) {
   return (Math.floor( ( float + 1.25 ) * 0.4 ) / 0.4)
 }
+
+
+// should i do it like this?
+//
+// Number.prototype.toTwoPointFive = function( this: number ) {
+// return ( Math.floor ( ( this + 1.25 ) * 0.4 ) / 0.4 )
+// }
+//
+// and then call values like this ?
+//
+// 11.38.toTwoPointFive()
+//
+// is this even legal ?
